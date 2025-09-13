737eeac5428f99bcd1a5875f2b2d36f1e40fab27
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-09-14 00:26:16 +0900

    :zap: pavementの仕様変更に伴う修正

--- R/data-convoluted-network.R
+++ R/data-convoluted-network.R
@@ -2,10 +2,12 @@ library(pavement)
 library(jpaccidents)
 library(sf)
 
+# 緯度・経度の設定
 center_lon <- 139.728187
 center_lat <- 35.679636
 width <- height <- 0.02
 
+# 道路データの取得
 bounding_box <- create_bbox(
   center_lon = center_lon,
   center_lat = center_lat,
@@ -13,22 +15,24 @@ bounding_box <- create_bbox(
   height     = height
 )
 roads <- fetch_roads(bounding_box) |>
-  st_transform(crs = 6677)
+  transform_to_cartesian()
 
+# 事故データの取得とフィルタリング
 download_path <- download_accident_data("main")
 accident_data <- read_accident_data(download_path)
-accident_data <- read_accident_data("honhyo_2023.csv")
 filter_area <- bounding_box |>
   convert_bbox_to_polygon(crs = st_crs(accident_data$accident))
 filtered_data <- accident_data$accident |>
   st_filter(filter_area) |>
-  st_transform(crs = 6677)
+  transform_to_cartesian()
 
+# 道路ネットワークの作成とセグメント化と事故データの割り付け
 road_network <- create_road_network(roads) |>
   set_events(filtered_data)
 segmented_network <- road_network |>
   create_segmented_network(segment_length = 10)
 
+# 畳み込み（非常に時間がかかります）
 convoluted_network <- segmented_network |>
   convolute_segmented_network(bandwidth = 100)
 saveRDS(convoluted_network, "data/convoluted_network.rds")
